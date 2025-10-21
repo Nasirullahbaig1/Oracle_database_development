33c23270627eccbbd08aa8af59af3a3b011c83d4
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-21 21:40:53 +0500

    Create practice.sql

---
+++ practice.sql
@@ -0,0 +1,136 @@
+--course
+--student
+
+CREATE TABLE course (
+    course_id     NUMBER,
+    course_name   VARCHAR2(50) NOT NULL,
+    course_credit NUMBER NOT NULL,
+    CONSTRAINT pk_course_id PRIMARY KEY ( course_id )
+);
+desc course;
+select * from course;
+
+CREATE TABLE student (
+    student_id      NUMBER,
+    studnet_name    VARCHAR2(50) NOT NULL,
+    student_fname   VARCHAR2(50),
+    assigned_course NUMBER,
+    CONSTRAINT pk_student_id PRIMARY KEY ( student_id ),
+    CONSTRAINT fk_assigned_course FOREIGN KEY ( assigned_course )
+        REFERENCES course ( course_id )
+);
+desc student;
+select * from student;
+
+commit;
+
+INSERT into course(COURSE_ID, COURSE_NAME, COURSE_CREDIT)
+values(1001, 'Programming fundementals', 3); 
+INSERT into course(COURSE_ID, COURSE_NAME, COURSE_CREDIT)
+values(1002, 'Machine Learning', 3);
+INSERT into course(COURSE_ID, COURSE_NAME, COURSE_CREDIT)
+values(1003, 'Islamic Studies', 2);
+INSERT into course(COURSE_ID, COURSE_NAME, COURSE_CREDIT)
+values(1004, 'Professional Practices', 3);
+INSERT into course(COURSE_ID, COURSE_NAME, COURSE_CREDIT)
+values(1005, 'Artificial Inteligence', 4); 
+select * from course;
+
+desc student;
+
+alter table student
+add enrolment_date date;
+
+alter table student
+add constraint CK_enrolment_date check (enrolment_date > date'2020-01-01');
+
+INSERT into student(STUDENT_ID, STUDNET_NAME, STUDENT_FNAME, ASSIGNED_COURSE, ENROLMENT_DATE)
+values(01, 'Nasir Ullah Baig', 'Karim Baig', 1001, date'2021-10-04'); 
+INSERT into student(STUDENT_ID, STUDNET_NAME, STUDENT_FNAME, ASSIGNED_COURSE, ENROLMENT_DATE)
+values(02, 'faizan Alam', 'Rehmat Juan', 1001, date'2021-10-04');
+INSERT into student(STUDENT_ID, STUDNET_NAME, STUDENT_FNAME, ASSIGNED_COURSE, ENROLMENT_DATE)
+values(03, 'Mujtaba', 'Adil', 1002, date'2020-10-04');
+select * from student;
+
+CREATE TABLE new_student (
+    student_id      NUMBER,
+    studnet_name    VARCHAR2(50) NOT NULL,
+    student_fname   VARCHAR2(50),
+    assigned_course NUMBER,
+    CONSTRAINT pk_student_id_new PRIMARY KEY ( student_id ),
+    CONSTRAINT fk_assigned_course_new FOREIGN KEY ( assigned_course )
+        REFERENCES course ( course_id )
+);
+
+alter table new_student
+add enrolment_date date;
+
+commit;
+
+select * from new_student;
+
+--merge table student into new student
+--target table and sourse table
+
+merge into new_student t
+using student s
+on (s.student_id = t.student_id)
+when not matched then
+insert values (s.student_id, s.STUDNET_NAME, s.STUDENT_FNAME, s.ASSIGNED_COURSE, s.ENROLMENT_DATE)
+when matched then
+update set t.STUDNET_NAME=s.STUDNET_NAME, t.STUDENT_FNAME=s.STUDENT_FNAME, t.ASSIGNED_COURSE=s.ASSIGNED_COURSE, t.ENROLMENT_DATE=s.ENROLMENT_DATE;
+
+commit;
+
+update student
+set student_fname = 'Adil khan'
+where student_id = 3;
+
+select * from student;
+
+commit;
+
+CREATE TABLE new_course (
+    course_id     NUMBER,
+    course_name   VARCHAR2(50) NOT NULL,
+    course_credit NUMBER NOT NULL,
+    CONSTRAINT pk_course_id_new PRIMARY KEY ( course_id )
+);
+
+select * from new_course;
+
+merge into new_course t
+using course s
+on (s.course_id=t.course_id)
+when not matched then
+insert values(s.course_id, s.course_name, s.course_credit)
+when matched then
+update set t.course_name=s.course_name, t.course_credit=s.course_credit;
+
+select * from new_course;
+
+update course
+set course_credit = 4
+where course_id = 1001;
+
+commit;
+
+drop table new_course;
+drop table new_student;
+
+commit;
+
+create table teacher(
+    teacher_id number,
+    teacher_name varchar2(50),
+    teacher_course number,
+    constraint PK_teacher_id primary key (teacher_id),
+    constraint FK_teacher_course foreign key (teacher_course) references course(course_id)
+);
+commit;
+
+
+
+
+
+
