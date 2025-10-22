d5825835fae16a0753d180bcab49f1153529a482
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-22 19:41:04 +0500

    created two new table to perform join function

--- practice2.sql
+++ practice2.sql
@@ -22,6 +22,38 @@ order by enrolment_date
 offset 1 row
 fetch next 2 row only;
 
+select * from student
+order by enrolment_date
+fetch first row with ties;
+
+create table employee(
+    e_no number, 
+    e_name varchar2(50) not null,
+    e_adress varchar2(150),
+    constraint PK_e_no primary key (e_no)
+);
+
+create table department(
+    dep_no number,
+    dep_name varchar2(50),
+    e_no number,
+    constraint PK_dep_no primary key (dep_no),
+    constraint FK_e_no foreign key (e_no) references employee(e_no)
+);
+
+select * from employee;
+select * from department;
+
+commit;
+
+
+
+
+
+
+
+
+
 
 
 
