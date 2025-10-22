c58d1265769cf51ccc2a04ede6dc757d85348be1
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-22 18:38:08 +0500

    Update practice2.sql

--- practice2.sql
+++ practice2.sql
@@ -4,3 +4,13 @@ select * from teacher;
 
 select * from student
 order by enrolment_date desc;
+
+update student
+set enrolment_date = date'2023-02-11'
+where student_id = 1;
+
+select * from student
+order by enrolment_date;
+
+select * from student
+order by enrolment_date desc;
