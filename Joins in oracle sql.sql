84231a3e6917c931064c7b4a307aac31d0e11c65
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-22 20:44:28 +0500

    Update Joins in oracle sql.sql

--- Joins in oracle sql.sql
+++ Joins in oracle sql.sql
@@ -36,14 +36,15 @@ values(1001, 'computer', 6);
 select * from department;
 
 --JOin = cross product + some conditions
+
 --Natural join
+--Natural join compare the common attribute from both table and give rows where both the attribute matches
 select e_name from employee  emp, department dep
 where emp.e_no=dep.e_no;
 --This is the logic behind natural join we take the product of two tables and then put some condition there.
 select e_name, dep_name from employee natural join department;
 --This is the actual syntex for natural join 
 commit;
-
 delete from department where e_no=6;
 
 
