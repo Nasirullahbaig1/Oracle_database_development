216d62d66b363df086efb129e523318f5eda94d4
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-23 21:40:23 +0500

    outer joins

--- HR_USER.sql
+++ HR_USER.sql
@@ -16,12 +16,43 @@ fetch first 1 rows only;
 /
 select * from employees
 order by salary desc;
-
-select employee_id, first_name, last_name, job_title 
+/
+--Equi join is a type of inner join the only difference is here 
+--we can only use equal sign
+select emp.employee_id, emp.first_name, emp.last_name, jb.job_title 
 from employees emp join jobs jb
 on emp.job_id=jb.job_id
 where jb.job_title = 'Stock Manager';
-
+/
+--Natural join is also a type or sub part of inner join
+select emp.employee_id, emp.first_name, emp.last_name, jb.job_title
+from employees emp natural join jobs jb;
+/
+--A LEFT OUTER JOIN is conceptually identical to a LEFT JOIN.
+--Showing all rows from the left table and matching rows from the right table
+select emp.employee_id, emp.first_name, emp.last_name, jb.job_title
+from employees emp left outer join jobs jb
+on emp.job_id=jb.job_id;
+/
+--the RIGHT JOIN (also called RIGHT OUTER JOIN)
+--It returns all records from the right table and only the matching records from the left table.
+select emp.employee_id, emp.first_name, emp.last_name, jb.job_title
+from employees emp right outer join jobs jb
+on emp.job_id=jb.job_id;
+/
+select *
+from employees emp left outer join jobs jb
+on emp.job_id=jb.job_id;
+/
+select *
+from employees emp right outer join jobs jb
+on emp.job_id=jb.job_id;
+/
+--The FULL JOIN (or FULL OUTER JOIN) in SQL returns all rows from both tables
+--
+select emp.employee_id, emp.first_name, emp.last_name, jb.job_title
+from employees emp full join jobs jb
+on emp.job_id=jb.job_id;
 
 
 
