6e747363a20f11d46466c2ba01ee31b95c8cc179
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-24 05:10:25 +0500

    Update HR_USER.sql

--- HR_USER.sql
+++ HR_USER.sql
@@ -54,21 +54,27 @@ select emp.employee_id, emp.first_name, emp.last_name, jb.job_title
 from employees emp full join jobs jb
 on emp.job_id=jb.job_id;
 /
+/
+/
+--the task below was to create a new table using ctas command from employee table
+--pick 5 columns where the department id is not null then create the table
+--next is update the the new table salary applying the condtion 
+--set the salary of employee to 20000 whoes salary is less then 10000.
 select * from employees;
-select * from employees where department_id is not NULL
+select * from employees where department_id is not NULL;
 /
 create table new_employee_nasir as select EMPLOYEE_ID, FIRST_NAME, LAST_NAME, HIRE_DATE, SALARY, DEPARTMENT_ID 
 from employees
 where DEPARTMENT_ID is not NULL;
 select * from new_employee_nasir;
-
+/
 update new_employee_nasir
 set salary = 20000
 where salary < 10000;
 commit;
-
+/
 select * from new_employee_nasir
-where salary = 20000
+where salary = 20000;
 
 
 
