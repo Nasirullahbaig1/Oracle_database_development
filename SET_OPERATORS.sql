629798adf5c9db5fcb447c8274a57d22930bef89
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-28 01:41:15 +0500

    Update SET_OPERATORS.sql

--- SET_OPERATORS.sql
+++ SET_OPERATORS.sql
@@ -10,5 +10,24 @@
     --combine result from both quries
     --not nessory to be distinct
 --MINUS
+    --
 --INTERSECT
+
+select * from employees;
+info employees;
+
+create table emp1 as select EMPLOYEE_ID, FIRST_NAME, SALARY from employees;
+select * from emp1;
+info emp1;
+
+create table emp2 as select EMPLOYEE_ID, FIRST_NAME, SALARY from employees
+where salary > 10000;
+select * from emp2;
+
+update emp1
+set  FIRST_NAME = 'steve'
+where EMPLOYEE_ID <= 120;
+
+drop table emp2;
+
 
