978fcde7f6cddd16d7dc29c5af3c5b95cf1dd602
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-27 23:19:55 +0500

    Update SET_OPERATORS.sql

--- SET_OPERATORS.sql
+++ SET_OPERATORS.sql
@@ -1,3 +1,14 @@
 --set operators
 --set operators combine the result of two queries
 --query with set operator is called compound query
+
+--type of operators
+--UNION
+  --combine result of two queries
+  --distinct records
+--UNION ALL
+    --combine result from both quries
+    --not nessory to be distinct
+--MINUS
+--INTERSECT
+
