ae7e1e04778bae41a1db12f4882a8e0cdd1d75c6
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-28 20:44:57 +0500

    Update Comparision operators.sql

--- Comparision operators.sql
+++ Comparision operators.sql
@@ -9,3 +9,10 @@
 --[NOT] IN : Equal to any memeber of the list or query
 --[NOT] EXIST : If subquery of exist return any records then true
 --IS [NOT] NULL : Check if the value is null and return ture if its null
+
+--Logical operators
+--AND - Return true if both the conditions are true
+--OR - Return true if either condition is true
+--NOT - Return false if the following conditions is true
+      --Return true if the following conditions are false
+      
