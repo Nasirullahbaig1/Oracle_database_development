4b1fbdaa63dd0122164cbbb8fd4112a5252cf7ef
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-10-31 19:01:54 +0500

    Update SQL FUNCTIONS.sql

--- SQL FUNCTIONS.sql
+++ SQL FUNCTIONS.sql
@@ -6,3 +6,6 @@
     --EXPLICIT data type conversion
         --Here we explicitly convert the data type.
         --TO CHAR:- Convert date, number and time stamp to character mostly used to fromat the data and display.  
+        select to_char (123) from dual;
+        select to_char (date'2021-03-12') from dual;
+        select to_char (date'2021-03-12', 'dd-mm-yyyy') from dual;
