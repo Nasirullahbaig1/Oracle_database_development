88051a0fcc93869874b26e6fd69ff66f7a1943d7
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-03 22:44:29 +0500

    Update Charactar functions.sql

--- Charactar functions.sql
+++ Charactar functions.sql
@@ -1,1 +1,19 @@
 --CHARACTAR FUNCTION IN ORACLE
+--these function deal with strings take strings as input and give strings as output
+
+--LOWER:- This function changes the upper case latters to lower case
+--syntax: lower(string)
+select lower(first_name) from employees;
+--it return all the first name column in lower case.
+
+--UPPER:- This function converst the string into upper case.
+--syntax: upper(string)
+select upper(first_name) from employees;
+--this querry convert the whole first column into upper case
+--we can use these two function while doing case sensitive searches.
+
+--
+
+
+
+
