8c7d1890993d85a255e34978e630c6c1dc4a4b18
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-03 20:09:44 +0500

    Update conditional functions.sql

--- conditional functions.sql
+++ conditional functions.sql
@@ -1,5 +1,6 @@
 --CONDITIONAL FUNCTIONS
 select * from employees;
+
 --DECODE:- it accepts one expression if the value matches with the first search parameters then returns the first result.
 --if it matches with the second search parameter then returns the second result and so on.
 select employee_id, first_name,
@@ -8,6 +9,18 @@ decode(department_id, 80, 'Belong to HR dep', 50, 'Belong to IT dep', 'unknown')
 --it searches dep_id column for the given if in dep_id and give the related result
 --then it will move to the second sreach and compare it in the column and then give that result also.
 --NOTE: usually null != to another null but in the case of decode null = null.
+--because decode function uses is null clause while comparing two nulls. 
+
+--CASE EXPRESSION:- It is same as decode but with more flexible syntax.
+select employee_id, first_name,
+case department_id 
+when 80 then 'Belong to HR dep'
+when 50 then 'Belong to IT dep'
+else 'unknown'
+end as discription
+from employees;
+--the key difference between these two functions if the flexlibility here we have more options to add conditons.
+
 
 
 
