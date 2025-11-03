a41ab9478b7bd3cfce6fcfe8a8e3d8ff215fb560
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-03 19:57:57 +0500

    Update conditional functions.sql

--- conditional functions.sql
+++ conditional functions.sql
@@ -1,9 +1,14 @@
 --CONDITIONAL FUNCTIONS
-
+select * from employees;
 --DECODE:- it accepts one expression if the value matches with the first search parameters then returns the first result.
 --if it matches with the second search parameter then returns the second result and so on.
-select decode(department_id, 80, 'Belong to HR dep', 50, 'Belong to IT dep') from employees;
+select employee_id, first_name,
+decode(department_id, 80, 'Belong to HR dep', 50, 'Belong to IT dep', 'unknown') as discription from employees;
 --The above querry performs two functions 
 --it searches dep_id column for the given if in dep_id and give the related result
 --then it will move to the second sreach and compare it in the column and then give that result also.
+--NOTE: usually null != to another null but in the case of decode null = null.
+
+
+
 
