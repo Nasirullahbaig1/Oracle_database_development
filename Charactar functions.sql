ab5f3ba73a953fdb72f574106b50e52f97eee49f
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-03 23:53:05 +0500

    Update Charactar functions.sql

--- Charactar functions.sql
+++ Charactar functions.sql
@@ -12,7 +12,15 @@ select upper(first_name) from employees;
 --this querry convert the whole first column into upper case
 --we can use these two function while doing case sensitive searches.
 
---
+--INITCAP:- Returns the input string in the initial character as upper case.
+--syntax: INITCAP(STRING)
+select * from employees;
+select initcap(first_name) from employees;
+select initcap('cow is on the road') from dual;
+
+--LENGHT:- return the lenght of the given string.
+--syntax: LENGHT(SRRING)
+select EMPLOYEE_ID, FIRST_NAME, LENGHT(last_name) from employees;
 
 
 
