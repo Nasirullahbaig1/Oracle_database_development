57ea9b6ff5a46d908790f946554992ea1ca5bc27
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-05 00:22:46 +0500

    Update Index.sql

--- Index.sql
+++ Index.sql
@@ -1,2 +1,6 @@
 --INDEX IN ORACLE SQL
+--is the object of the database in which all the indexed column values are 
+--stored along with the location of the row.
+--for example: telephone directory like the names are in order and in index its telling us from where to where the names start and end.
+
 
