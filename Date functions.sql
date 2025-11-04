37b2b74fa268dfebe2976df2ffafe25f8fb418ee
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-04 23:46:58 +0500

    Update Date functions.sql

--- Date functions.sql
+++ Date functions.sql
@@ -29,6 +29,28 @@ select months_between(sysdate, date'2002-03-18') from dual;
 select next_day(sysdate, 'sun') from dual;
 --this function tell us the when is the next sunday from system date or current date or given date.
 
+--TRUNC:- trunctes the date with the given unit.
+--syntax: TRUNC(INPUT DATE, UNIT)
+select trunc(sysdate), sysdate from dual;
+select trunc(sysdate, 'month'), sysdate from dual;
+select trunc(sysdate, 'year'), sysdate from dual;
+
+--ROUND:- Roundoff the date with the given unit.
+--syntax: ROUND(INPUT DATE, UNIT)
+select round(sysdate), sysdate from dual;
+select round(sysdate, 'month'), sysdate from dual;
+select round(sysdate, 'year'), sysdate from dual;
+
+--how we can add day on out given date
+select sysdate, sysdate+4 from dual; --added 1 day
+select sysdate, sysdate+1/24 from dual; --added 1 hour in the given date.
+
+select add_months(sysdate, 10) from dual; --add months
+select add_months(sysdate, -10) from dual; --subtract months
+
+
+
+
 
 
 
