5eae04feb4d254a3ac732e4c4813622e25b2d9e0
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-04 22:00:46 +0500

    Update Date functions.sql

--- Date functions.sql
+++ Date functions.sql
@@ -1,1 +1,15 @@
 --DATE FUNCTIONS IN ORACLE
+
+--SYSDATE:- Returns current date and time of the oracle server.
+alter session set nls_date_format= 'dd-mm-yyyy hh12:mi:ss';
+select sysdate from dual;
+select current_date, sysdate from dual;
+
+select systimestamp from dual;
+select current_timestamp from dual;
+
+
+
+
+
+
