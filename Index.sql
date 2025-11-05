1f6a977753fa5f600f41a67abb37aec27a635039
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-05 20:00:44 +0500

    Update Index.sql

--- Index.sql
+++ Index.sql
@@ -11,7 +11,9 @@
 --This is also called binery tree which means one parents can can two children.
 --B-tree indexing do not store the null values.
 --B-tree can have maximum 32 columns.
-
+--syntax: create [UNIQUE] index <index_name>
+--        on <table_name>
+--        (column1/expression1 >[<column2/expression2>, .....]);
 
 --Bitmap:- It is used when distinct values of the indexed columns are limited.
 --Uses bitmap to point the values.
