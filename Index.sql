ee7b62ffe6749f55954000b663b54887c5ec8f7a
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-05 19:19:06 +0500

    Update Index.sql

--- Index.sql
+++ Index.sql
@@ -3,4 +3,16 @@
 --stored along with the location of the row.
 --for example: telephone directory like the names are in order and in index its telling us from where to where the names start and end.
 
+--types of index
+
+--B-tree:- Defalut index.
+--values are stored in a balanced tree format.
+--Good for primary key
+--This is also called binery tree which means one parents can can two children.
+
+--Bitmap:- It is used when distinct values of the indexed columns are limited.
+--Uses bitmap to point the values.
+--usually used on a data like gender where most of the data is male or female.
+
+ 
 
