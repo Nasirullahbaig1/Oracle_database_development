1da855a351f471ab4ce96518302990dcc32f1b54
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-06 19:24:00 +0500

    sequance

---
+++ sequance.sql
@@ -0,0 +1,9 @@
+--SEQUANCE IN ORACLE
+--sequance is an object which can generate a unique number.
+--Most used for creating a unique number for the primary key coulmn.
+--Sequance number is incemented whenever any value is 
+--generated out of it even when a trasaction is committed or rolled back.
+
+--NEXTVAL(give next value of the sequance) and CURRVAL(give the current value of the sequance)
+--can be used on a sequance.
+
