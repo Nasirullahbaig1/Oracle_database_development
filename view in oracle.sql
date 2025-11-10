4634cc03e6075292af8db4c2d4279c4234026905
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-10 20:46:01 +0500

    Update view in oracle.sql

--- view in oracle.sql
+++ view in oracle.sql
@@ -1,3 +1,10 @@
 --VIEW IN ORACLE
 --view is the logical table which represents another table or view.
 --view does not store any sort of data. its a virtual table or named query.
+
+--view provides security of the and it is also used for simplicity.
+--updatable and non-updatable views.
+
+--However if we have used an aggregate function, join, union(or any set operator), 
+--distinct or group by then such a view cannot be updated.
+
