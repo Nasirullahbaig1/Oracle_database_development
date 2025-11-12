4f4baed250c7ada1d49f75488f4c064ec5ccf1af
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-12 19:49:50 +0500

    Update synonyms.sql

--- synonyms.sql
+++ synonyms.sql
@@ -1,3 +1,9 @@
 --SYNONYM IN ORACLE
 --synonym is the alternative name of the object like table, view, sequance, procedure, etc.
 --CREATE SYNONYM or CREATE ANY SYNONYM privalige are required.
+--mostly used when object of another schema need to be accessed.
+
+--CREATE [OR REPLACE][PUBLIC] SYNONYM
+--[schema] synonym_name
+--FOR [schema.] object_name [@dblink]
+
