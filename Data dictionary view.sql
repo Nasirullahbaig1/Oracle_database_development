a3c9fec6094e97272290d04543a77b9bbac037f1
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-13 21:23:09 +0500

    Update Data dictionary view.sql

--- Data dictionary view.sql
+++ Data dictionary view.sql
@@ -2,3 +2,13 @@
 --there are data dictionary tables maintained by oracle but they are not directly
 --accessible to us. we can access the view privided by oracle with read only 
 --access on those tables.
+
+--TYPES OF DATA DICTIONARY VIEWS
+--ALL_
+    --all innformation are accesable to the user.
+    --own schema + object of other schemas on wchich which current user has access to via any user.
+    
+--USER_
+    --information on objects own by user'
+    --own schema only
+--DBA_
