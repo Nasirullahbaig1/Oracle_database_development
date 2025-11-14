34f9ebf7bb2cec641e5fc1f67c273c0f2a717cdf
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-15 00:04:16 +0500

    Update Miscellaneous topic.sql

--- Miscellaneous topic.sql
+++ Miscellaneous topic.sql
@@ -4,3 +4,13 @@
 --We can use it multiple time in the same query.
 --It help to organize and simplify your code.
 --sytax: cte_name [(column_name),[col_name]....)] as (subquery)
+
+--WITH cte_name AS (
+--    -- Your CTE query goes here
+--    SELECT column1, column2, ...
+--    FROM table_name
+--    WHERE condition
+--)
+--SELECT * 
+--FROM cte_name;
+
