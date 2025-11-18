bd46d7dd389ec94fa3ad36faa22afd65cbe8260c
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-18 20:07:02 +0500

    Update Distinct.sql

--- Distinct.sql
+++ Distinct.sql
@@ -10,7 +10,18 @@ select distinct manager_id from employees where manager_id is not null order by 
 --we simply use the distinct function to retrive uniue values from the data.
 --when we apply the distinct function in a select function it will apply on all the selected columns.
 
---
+--DEFALUT IN CONVERSION FUNTION
+--When data type conversion funtions are used with wrong input and conversion fails then
+--a default value will be assigned to it.
+--can be used with to_ and CAST funtions.
+
+--Lets have an example 
+--when in a date column we add a date 30 febrary which means it dosent exist in reality
+--in this situation our converion function also fails to interpret this 
+--here we use default conversion functions to tackle the problem.
+
+
+
 
 
 
