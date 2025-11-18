2f1069aaefab4a50fef2907c8f15ab4639425e0d
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-18 19:50:41 +0500

    Update Distinct.sql

--- Distinct.sql
+++ Distinct.sql
@@ -1,1 +1,16 @@
 --DISTINCT
+--used in select clause
+--it filters out the duplicate rows based on columns/columns 
+--mentioned in the SELECT clause and provide a single copy of the result.
+
+select * from employees;
+select manager_id from employees where manager_id is not null order by manager_id;
+select distinct manager_id from employees where manager_id is not null order by manager_id;
+
+
+
+
+
+
+
+
