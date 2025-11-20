4d3fe813734cd0016cfa900efa72fd4b49242ee6
Nasir Ullah Baig <baignasirullah1@gmail.com>
2025-11-20 19:57:41 +0500

    Update Distinct.sql

--- Distinct.sql
+++ Distinct.sql
@@ -71,11 +71,18 @@ where first_name like '%r%'; --This will give us the name contain r in the middl
 --SELECT column_list FROM...
 
 
---TRANSFORMMING COLUMNS INTO ROWS
+--TRANSPOSTING COLUMNS INTO ROWS
 --Here we change the rows into columns and coumns into rows. 
 --PIVOT:
 --This clause is used to respose rows into columns. data like key-value pair can be concerted into columns.
---
+--syntax:
+--SELECT * FROM 
+--  (your query with same data in rows)
+--PIVOT
+--(
+--  AGGRIGATE_FUCNTION()
+--  FOR COLUMN_NAME IN()
+--)
 
 
 
